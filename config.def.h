0b201e6c3ae7481a28ccac7d67b39a59db989066
gingjin <gingjin@163.com>
2024-03-26 15:11:33 +0800

    feat: add patch: movestack

--- config.def.h
+++ config.def.h
@@ -80,6 +80,7 @@ static const char *decbri[]  = { "xbacklight", "-dec", "5", NULL };
 /* slock */
 static const char *slock[]  = { "slock", NULL };
 
+#include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ 0,         XF86XK_AudioRaiseVolume,      spawn,          {.v = incvol } },
@@ -93,6 +94,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
