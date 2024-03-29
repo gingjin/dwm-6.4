9ef388e111b9af8a54f9954032f727fe76537d26
gingjin <gingjin@163.com>
2024-03-29 23:44:14 +0800

    feat: added rofi command

--- config.def.h
+++ config.def.h
@@ -63,6 +63,8 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+/* rofi */
+static const char *rofi[]  = { "rofi", "-show", "drun", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
